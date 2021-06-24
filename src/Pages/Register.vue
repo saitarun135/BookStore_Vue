bf8b404685eefbf01c4e99fcda9f834c9005a52b
saitarun135 <saitarun135@gm.com>
2021-06-24 19:06:03 +0530

    feat: changing names into camelCase and clear form issue and toggling features are added

--- src/Pages/Register.vue
+++ src/Pages/Register.vue
@@ -1,6 +1,5 @@
 <template>
 <div class="main">
-
     <div class="container">
         <img src="../assets/sideImg.png" alt="notFound" />
         <p>Online Book Shopping</p>
@@ -9,32 +8,73 @@
                 <h5 class="signin">Login</h5>
                 <h5 class="signup">signup</h5>
             </div>
-            <form>
+            <form ref="myForm" @submit.prevent="handlesubmit">
                 <div class="fullname">
                     <p>FullName</p>
-                    <input type="text" class="namebox" required v-model="name" autocomplete="off" pattern="[A-Za-z]{3,12}">
+                    <input type="name" class="namebox"  required v-model="fullName" autocomplete="off" pattern="[A-Za-z]{3,12}">
                 </div>
                 <div class="username">
                     <p>EmailID</p>
-                    <input type="email" class="emailbox" required v-model="email" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
+                    <input type="email" class="emailbox" required v-model="emailID" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
                 </div>
                 <div class="pass">
                     <p>Password</p>
-                    <input type="password" class="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$" v-model="password" required>
+                    <input :type="password_type" class="password" id="passField" v-model="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$" required>
+                    <i class="bi bi-eye-slash" id="togglePassword" @click="togglePassword();"></i>
                 </div>
-
                 <div class="mobile">
                     <p>MobileNumber</p>
-                    <input type="tel" class="telephone" pattern="^\d{10}$" required>
+                    <input type="tel" class="telephone" v-model="mobile" pattern="^\d{10}$" required>
                 </div>
-                <button class="btn-section" type="submit">Signup</button>
+                <button class="btn-section"  type="submit">Signup</button>
             </form>
         </div>
     </div>
-
 </div>
 </template>
 
+<script>
+import service from '../service/User'
+export default {
+    name: 'Register',
+    data() {
+        return {
+            fullName: '',
+            emailID: '',
+            password: '',
+            mobile: '',
+            password_type: "password",
+        }
+    },
+    methods: {
+        togglePassword() {
+            this.password_type = this.password_type === 'password' ? 'text' : 'password'
+            document.getElementById('passField').style.margin='0px 0px 0px 69px';
+            document.getElementById('passField').style.width="252px";
+            document.getElementById('passField').style.height="35px";
+            document.getElementById('passField').style.background="$pale_white 0% 0% no-repeat padding-box";
+        },
+        
+        async handlesubmit() {
+            let userData = {
+                fullName: this.fullName,
+                emailID: this.emailID,
+                password: this.password,
+                mobile: this.mobile
+            }
+            service.userRegister(userData).then(response => {
+                alert("user registered successfully");
+                this.$refs.myForm.reset();
+                return response;                
+            }).catch(error => {
+                if(error.response==422){alert("internal server issue");}
+                return error;
+            })
+        }
+    }
+}
+</script>
+
 <style lang="scss" scoped>
-    @import "@/styles/Register.scss";
+@import "@/styles/Register.scss";
 </style>
