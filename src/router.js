785924b0d6b6d50f97a73fbb59fe05ae952dad9b
saitarun135 <saitarun135@gm.com>
2021-06-28 13:18:17 +0530

    build: path added for forgotpassword component

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Register from './Pages/Register.vue'
 import Login from './Pages/Login.vue'
+import Forgot from './Pages/Forgot.vue'
 Vue.use(Router)
 export  default new Router({
     mode:'history',
@@ -13,6 +14,10 @@ export  default new Router({
         {
             path:'/login',
             component:Login
+        },
+        {
+            path:'/forgot',
+            component:Forgot
         }  
         ]
 })
