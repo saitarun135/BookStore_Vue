6855c087c02d75178e5334de0556fcc19f4150f7
saitarun135 <saitarun135@gm.com>
2021-07-04 18:21:51 +0530

    build: selectbar and Books name added

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,7 +2,7 @@
 <div class="main">
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="navbar-header">
-            <img src="../assets/education.png"  alt="notFound" />
+            <img src="../assets/education.png" alt="notFound" />
         </div>
         <ul class="nav navbar-nav">
             <li>
@@ -12,24 +12,38 @@
         <div class="input-group">
             <i class="fas fa-search"></i>
             <div class="form-outline">
-                <input type="search"  class="form-control" placeholder='search...' />
+                <input type="search" class="form-control" placeholder='search...' />
             </div>
         </div>
-       
-        <ul class="nav navbar-nav navbar-right" id="right-bar">
-             <li><a> <i class="far fa-user"></i></a></li>
-             <p class="profile-content">profile</p>
-             <li><a><i class="fas fa-cart-plus"></i></a></li>
-             <p class="cart-content">cart</p>
-         </ul>
-      
-    </nav>
 
+        <ul class="nav navbar-nav navbar-right" id="right-bar">
+            <li><a> <i class="far fa-user"></i></a></li>
+            <p class="profile-content">profile</p>
+            <li><a><i class="fas fa-cart-plus"></i></a></li>
+            <p class="cart-content">cart</p>
+        </ul>
+    </nav>
+    <div class="mid-body">
+        <h6>Books<span class="items">(128items)</span></h6>
+        <select  class="options">
+            <option value="volvo">sort by relevance</option>
+        </select>
+    </div>
+    <DisplayBooks />
 </div>
 </template>
 
 <script>
+import DisplayBooks from './DisplayBooks.vue'
+export default {
+    components: {
+        DisplayBooks
+    },
+    data() {
+        return {}
+    },
 
+}
 </script>
 
 <style lang="scss" scoped>
