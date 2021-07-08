c4fd23b7b2ec46cd09e468bb8d3e7135560598a3
saitarun135 <saitarun135@gm.com>
2021-07-08 08:52:39 +0530

    build: admin dashboard for admin users

---
+++ src/Admin/AdminDashboard.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="main">
+    <div class="navbar navbar-default navbar-fixed-top">
+        <div class="navbar-header">
+            <img src="../assets/education.png" alt="notFound" class="education-image" />
+        </div>
+        <ul class="nav navbar-nav">
+            <li>
+                <p class="brand">Bookstore</p>
+            </li>
+        </ul>
+        <div class="input-group">
+            <i class="fas fa-search"></i>
+            <div class="form-outline">
+                <input type="search" class="form-control" placeholder='search...' />
+            </div>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right" id="right-bar">
+            <li><a> <i class="far fa-user"></i></a></li>
+            <p class="profile-content">profile</p>
+        </ul>
+    </div>
+    <div class="mid-body">
+        <h6>Books<span class="items">(128items)</span></h6>
+        <select class="options">
+            <option value="volvo">sort by relevance</option>
+        </select>
+    </div>
+    <div class="create-book">
+        <CreateBook class="createBook" />
+    </div>
+
+</div>
+</template>
+
+<script>
+import CreateBook from './CreateBook.vue'
+export default {
+    components: {
+        CreateBook
+    },
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/AdminDashboard.scss";
+</style>
