0db864f23752d59daeb3ac6bc86b5f6033f8ddb2
saitarun135 <saitarun135@gm.com>
2021-07-12 21:04:39 +0530

    refactor: watcher propery added for cart length

--- src/components/sortBooksLowtoHigh.vue
+++ src/components/sortBooksLowtoHigh.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="carddisplay-section">
-    <div v-for="book in books" :key="book.id" class="card book">
+    <div v-for="book in books" v-bind:key="book.id" class="card book">
         <div class="image-section">
             <div class="image-container">
                 <img  v-bind:src="book.file" />
@@ -14,27 +14,27 @@
         </div>
         <div class="price-section">
             Rs. {{book.price}}<label class="default">(2000)</label>
-            <button v-if="flag" class="btn-grp" type="submit" @click="handlesubmit();Togglebtn();">close</button>
+            <!-- <button v-if="flag" class="btn-grp" type="submit" @click="handlesubmit();Togglebtn();">close</button> -->
         </div>
-        <div class="buttons">
-            <div class="button-groups">
-                <button type="button"  @click="toggle(book.id);flip(book.id);"  v-if="state==true" class="AddBag">Add to Bag</button>
-                <button v-if="state==true" class="wishlist">wishlist</button>
+         <div class="buttons">
+            <div class="button-groups"  v-if="!addedBooks.includes(book.id)">
+                <button type="submit"  @click="handleCart(book.id);toggle(book.id);addedBooks.push(book.id)"  class="AddBag">Add to Bag</button>
+                <button  class="wishlist">wishlist</button>
             </div>
-            <div v-if="state==false" class="AddedBag">
-                <button class="big-btn">Added to Bag</button>
+            <div class="AddedBag" v-else>
+            <button class="big-btn" @click="removeFromCart(book.id);addedBooks=addedBooks.filter(id=>id!==book.id)">Added to Bag</button>
             </div>
         </div>
     </div>
+    </div>
 
-</div>
 </template>
 
 <script>
 import service from '../service/User'
 
 export default {
-
+ 
     data() {
         return {
             result: 0,
@@ -45,18 +45,26 @@ export default {
             flag: true,
             state: true,
             clickedCard: '',
+             addedBooks:[],
             books: [{
-                id: 0,
-                file: 'https://images-na.ssl-images-amazon.com/images/I/41MdP5Tn0wL._SX258_BO1,204,203,200_.jpg',
-                name: 'Default Card',
-                author: 'Sai',
-                price: '..'
+                // id: 0,
+                // file: 'https://images-na.ssl-images-amazon.com/images/I/41MdP5Tn0wL._SX258_BO1,204,203,200_.jpg',
+                // name: 'Default Card',
+                // author: 'Sai',
+                // price: '..'
             }, ]
         }
     },
+     watch:{
+    addedBooks:{
+            handler(val){
+               this.$emit('update-books-count',val.length)
+             },
+             deep:true
+          }
+        },
     methods: {
-         toggle(id) {
-            
+         toggle(id) {  
             this.clickedCard = id;
             // this.card.content = this.notes.filter((note) => note.id === id);
           console.log(this.clickedCard);
@@ -68,12 +76,28 @@ export default {
         Togglebtn() {
             this.flag = !this.flag;
         },
-        handlesubmit() {
+        mounted() {
             service.userDisplayBooksLowtoHigh().then(response => {
                 this.books.push(...response.data);  
                 console.log(this.response);   
             })
         },
+        handleCart(bookId){
+            let userData={
+                id: bookId,
+            }
+            service.userUpdateCart(userData).then(response=>{
+                return response;
+            })
+        },
+        removeFromCart(bookId){
+            let userData={
+                id:bookId,
+            }
+            service.userRemoveFromCart(userData).then(response=>{
+                return response;
+            })
+        }
     }
 }
 </script>
