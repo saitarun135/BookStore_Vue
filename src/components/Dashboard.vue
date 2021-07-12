50347ce5218e64b1ead3d58f1298116b964f3bd3
saitarun135 <saitarun135@gm.com>
2021-07-12 21:06:38 +0530

    refactor: importing child components and cart functionality added

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="main">
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="navbar navbar-default navbar-fixed-top">
         <div class="navbar-header">
-            <img src="../assets/education.png" alt="notFound" />
+            <img src="../assets/education.png" alt="notFound" class="education-image" />
         </div>
         <ul class="nav navbar-nav">
             <li>
@@ -10,38 +10,74 @@
             </li>
         </ul>
         <div class="input-group">
-            <i class="fas fa-search"></i>
+            <i @click="handlesubmit();" class="fas fa-search"></i>
             <div class="form-outline">
-                <input type="search" class="form-control" placeholder='search...' />
+                <input type="search" v-model="name" class="form-control" placeholder='search...' />
             </div>
         </div>
 
         <ul class="nav navbar-nav navbar-right" id="right-bar">
             <li><a> <i class="far fa-user"></i></a></li>
             <p class="profile-content">profile</p>
-            <li><a><i class="fas fa-cart-plus"></i></a></li>
-            <p class="cart-content">cart</p>
+            <li @click="shownComponent='Cart'"><a><i class="fas fa-cart-plus"></i></a></li>
+            <p class="cart-content" >cart <span class="length" v-if="booksCount">{{booksCount}}</span></p>
+            
         </ul>
-    </nav>
+    </div>
     <div class="mid-body">
         <h6>Books<span class="items">(128items)</span></h6>
-        <select  class="options">
-            <option value="volvo">sort by relevance</option>
+
+        <select class="options" @change="applyOption">
+            <option disabled value="">Sort by relevance</option>
+            <option value="HighToLow">price:High to Low</option>
+            <option value="LowToHigh">price:Low to High</option>
         </select>
     </div>
-    <DisplayBooks />
+
+    <div v-if="flam==false">
+        <h2>Hello</h2>
+    </div>
+    <DisplayBooks v-show="flag==='noOrder' && shownComponent==='DisplayBooks'" @update-books-count="(n)=>booksCount=n"/>
+    <Cart v-show=" shownComponent==='Cart'"   />
+    <sortBooksLowtoHigh v-show="flag==='lowToHigh'" @update-books-count="(n)=>booksCount=n" />
+    <sortBooksHightoLow v-show="flag==='highToLow'" @update-books-count="(n)=>booksCount=n" />
+   
 </div>
 </template>
 
 <script>
+import sortBooksLowtoHigh from './sortBooksLowtoHigh.vue'
+import sortBooksHightoLow from './sortBooksHightoLow.vue'
 import DisplayBooks from './DisplayBooks.vue'
+import Cart from './Cart.vue'
 export default {
     components: {
-        DisplayBooks
+        DisplayBooks,
+        sortBooksLowtoHigh,
+        sortBooksHightoLow,
+        Cart
     },
     data() {
-        return {}
+        return {
+            shownComponent:'DisplayBooks',
+             booksCount:0,
+            flag: 'noOrder',
+            brand: 'Bookstore',
+            name: '',
+            flam: true,
+            visible: true,
+        }
     },
+    methods: {
+        flip() {
+            this.flam = !this.flam;
+        },
+        applyOption(evt) {
+            if (evt.target.value === "HighToLow") {
+                this.flag = 'highToLow';
+            } else this.flag = 'lowToHigh';
+        },
+    }
 
 }
 </script>
